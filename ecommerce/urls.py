e31cad76bb1787c3ae4fbb2f04556ef75cc45358
ADEL-MAHMOUD10 <adelam578568@gmail.com>
2025-08-05 08:02:42 +0300

    redesgin add product page

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path ,include
 from accounts import views
 from django.conf import settings
-from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home, name='home'),
